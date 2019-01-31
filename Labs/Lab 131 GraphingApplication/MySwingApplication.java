8cec157533559435e62de1ec55927b64573c6f4d
795965 <42558488+795965@users.noreply.github.com>
2019-01-31 10:33:44 -0800

    Started lab 131

---
+++ Labs/Lab 131 GraphingApplication/MySwingApplication.java
@@ -0,0 +1,16 @@
+
+/**
+ * Lab to create Graphing Application
+ *
+ * @author (Ziggy Sheynin)
+ * @version (Lab 131)
+ */
+import javax.swing.*;
+
+public class MySwingApplication implements Runnable
+{
+    public void run(){
+    }
+    public void main(){
+    }
+}
