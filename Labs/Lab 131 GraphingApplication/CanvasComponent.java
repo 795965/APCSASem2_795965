bc23f813dcdce4b130cc7b7dd89bc2b26ac59102
795965 <42558488+795965@users.noreply.github.com>
2019-02-06 10:16:41 -0800

    Activity 3... but still doesn't move

--- Labs/Lab 131 GraphingApplication/CanvasComponent.java
+++ Labs/Lab 131 GraphingApplication/CanvasComponent.java
@@ -7,23 +7,86 @@
  */
 import java.awt.*;
 import javax.swing.*;
-public class CanvasComponent extends JComponent
+import java.awt.event.*;
+public class CanvasComponent extends JComponent implements MouseListener, MouseMotionListener 
 {
 
-    int height;
-    int width;
-    int x;
-    int y;
+    int rectHeight;
+    int rectWidth;
+    int rectX;
+    int rectY;
+    int mouseFromX;
+    int mouseFromY;
+    boolean shapeSelected;
+    int mouseToX;
+    int mouseToY;
     public CanvasComponent(int width, int height){
+
         setSize(width, height);
-        height = 20;
-        width = 40;
-        x = 200;
-        y = 400;
+        rectHeight = 20;
+        rectWidth = 40;
+        rectX = 200;
+        rectY = 400;
+        this.addMouseListener(this);
+        this.addMouseMotionListener(this);
     }
-    
+
     public void paintComponent(Graphics g){
         g.setColor(Color.red);
-        g.fillRect(40, 30, 200, 400);
+        g.fillRect(400, 300, 40, 40);
+    }
+
+    //MouseListener Methods
+    public void mouseClicked(MouseEvent e){
+        //This method is called by Swing when a mouse button is pressed and released.
+    }
+
+    public void mousePressed(MouseEvent e){
+        //This method is called by Swing when a mouse button is pressed.
+        mouseFromX = e.getX();
+        mouseFromY = e.getY();
+        if(mouseFromX > rectX && mouseFromX < rectX + rectWidth &&
+        mouseFromY > rectY && mouseFromY < rectY + rectHeight){
+            shapeSelected = true;
+        }  else {
+            shapeSelected = false;
+        }
+    }
+
+    public void mouseReleased(MouseEvent e){
+        //This method is called by Swing when a mouse button is released.
+    }
+
+    public void mouseEntered(MouseEvent e){
+        //This method is called by Swing when the cursor moves inside the bounding box of
+        //the component that defines the MouseListener interface.
+    }
+
+    public void mouseExited(MouseEvent e){
+
+        //This method is called by Swing when the cursor moves outside the bounding box of
+        //the component that defines the MouseListener interface.
+    }
+    //mouseMotionListenermethods
+    public void mouseDragged(MouseEvent e){
+        //This method is called by Swing when a mouse button is pressed and then the mouse
+        //is moved with the button depressed.
+        if(shapeSelected == true){
+            mouseToX = e.getX();
+            mouseToY = e.getY();
+
+            rectX = rectX +(mouseToX - mouseFromX);
+            rectY = rectY + (mouseToY - mouseFromY);
+
+            mouseFromX = mouseToX;
+            mouseFromY = mouseToY;
+            repaint();
+            System.out.println("made it to MouseDragged");
+        }
+    }
+
+    public void mouseMoved(MouseEvent e){
+        //This method is called by Swing when the mouse is moved without any button
+        //depressed.
     }
 }
