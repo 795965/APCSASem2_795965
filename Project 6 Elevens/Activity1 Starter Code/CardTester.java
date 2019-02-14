8d1c779ff0e3254d44dff0199788a19f5f299ef1
795965 <42558488+795965@users.noreply.github.com>
2019-02-14 10:24:12 -0800

    Finished Activity 1

--- Project 6 Elevens/Activity1 Starter Code/CardTester.java
+++ Project 6 Elevens/Activity1 Starter Code/CardTester.java
@@ -3,11 +3,19 @@
  */
 public class CardTester {
 
-	/**
-	 * The main method in this class checks the Card operations for consistency.
-	 *	@param args is not used.
-	 */
-	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-	}
+    /**
+     * The main method in this class checks the Card operations for consistency.
+     *	@param args is not used.
+     */
+    public static void main(String[] args) {
+        /* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+        Card c1 = new Card("Spades", "7", 7);
+        Card c2 = new Card("Hearts", "7", 7);
+        Card c3 = new Card("Clubs", "7", 7);
+
+        System.out.println(c3.toString());
+        System.out.println(c2.matches(c1));
+
+    }
+
 }
