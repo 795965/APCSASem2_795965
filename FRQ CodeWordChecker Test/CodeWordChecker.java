cb84148334ddb4a297571b8b4df6a4030679400c
795965 <42558488+795965@users.noreply.github.com>
2019-03-07 09:48:31 -0800

    added codeword checker frq test

---
+++ FRQ CodeWordChecker Test/CodeWordChecker.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class CodeWordChecker here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class CodeWordChecker implements 
+{
+    public boolean isValid (String str){
+        if(String.length() < min || str.length() > max){
+        return false;
+    }
+    for(int i = 0; i < str.length; i++){
+        if(str.substring(i, i + not.length().equals(not)){
+            return false;
+        }
+    }
+    return true;
+}
+}
