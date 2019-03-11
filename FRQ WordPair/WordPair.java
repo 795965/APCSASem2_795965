e79d39254a75a62300ec62ecb18de10b7bb5cf8f
795965 <42558488+795965@users.noreply.github.com>
2019-03-11 10:03:28 -0700

    Almost finished FRQ
    FRQ doesn't compile

---
+++ FRQ WordPair/WordPair.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class WordPair here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class WordPair
+{
+    String [] words1;
+    public WordPair(String first, String second){
+        String [] words1 = new String[2];
+    }
+
+    public String getFirst(){
+       return words1 [0];
+    }
+
+    public String getSecond(){
+        return words1 [1];
+    }
+}
---
+++ FRQ WordPair/WordPairList.java
@@ -0,0 +1,36 @@
+
+/**
+ * Writing FRQ WordPair
+ *
+ * @author (Ziggy Sheynin)
+ * @version (3/11/19)
+ */
+
+import java.util.ArrayList;
+
+public class WordPairList 
+{
+    private ArrayList<WordPair> allPairs; //initializes all pairs arrayList
+   public WordPairList(String [] words){
+       allPairs = new ArrayList<WordPair>(); //assigns wordPair ArrayList to allPairs
+       for(int i = 0; i < words.length -1; i++){ 
+           for(int j = i+1; j < words.length; j++){ //nested for loop to traverse array words
+               allPairs. add(new WordPair(words[i], words[j])); //adds the two words to the arrayList all Pairs
+            }
+        }
+    }
+    
+    public int numMatches(){
+        int numMatches = 0; //counter
+        for(int i = 0; i < allPairs.size(); i++){ //traverses array
+            if(allPairs.getFirst().equals(allPairs.getSecond())){ //if the two words are the same
+                numMatches++; //counter adds one
+            }
+        }
+        return numMatches; //returns the number of times the words are the same
+    }
+    
+    public void main(){
+        
+    }
+}
