d4e913ed461808df8a0e684c2bcdb8a9f44fe041
795965 <42558488+795965@users.noreply.github.com>
2019-04-15 12:05:38 -0700

    415 arraylist and arrays

---
+++ ArrayList Array Review 415/someClass.java
@@ -0,0 +1,31 @@
+
+/**
+ * Write a description of class someClass here.
+ *
+ * @author (Ziggy Sheynin)
+ * @version (4/15)
+ */
+
+import java.util.*;
+
+public class someClass
+{
+
+    ArrayList <String> names = new ArrayList <String>();
+
+    public void sortStrings(ArrayList<String> names){
+        // sortStrings sorts the contents on names into ascending order
+        // You may use, insertion, selection, or merge sort
+        
+        
+    }
+
+    public void shuffleStrings(ArrayList<String> names){
+        // should shuffle the contents of names
+    }
+
+    public void insertString(String word){
+
+        //this method will insert the parameter "word" into the sorted array at the location where the arrayList remains sorted.
+    }
+}
